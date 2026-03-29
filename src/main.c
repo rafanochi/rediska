191b638174e249b6144748568b8e59eeb952940a
rafanochi <zawkindev@gmail.com>
2026-03-29 15:50:22 +0500

    trying: read_full, write_all

--- src/main.c
+++ src/main.c
@@ -1,11 +1,61 @@
+#include "sys/types.h"
 #include <arpa/inet.h>
+#include <assert.h>
 #include <errno.h>
 #include <netinet/ip.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+const size_t k_max_msg = 4096;
+
+size_t int32_t one_request(int connfd){
+  char rbuf[4+k_max_msg];
+  errno = 0;
+  int32_t err = read_full(connfd, rbuf, 4);
+  if (err) {
+    msg(errno==0? "EOF" : "read() err");
+    return err;
+  }
+
+
+}
+
+static int32_t read_full(int fd, char *buf, size_t n)
+{
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+
+    if (rv < 0) {
+      return -1;
+    }
+
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+
+  return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+  while (n > 0) {
+    size_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1;
+    }
+
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+
+  return 0;
+}
 
 static void do_something(int connfd)
 {
